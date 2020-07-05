4b3cf69a93847174293d1d106e2d44a488b2fd23
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-05 15:20:32 -0700

    provide user feedback and routing for successful/unsuccessful upload and cancel

---
+++ sprint-3/client/src/components/scenes/Success/Success.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Success = () => {
+  return (
+    <div className="success">
+      <main className="container">
+        <div className="success__message">
+          <div>Your video has been successfully uploaded. Return <Link to="/">home</Link></div>
+        </div>
+    </main>
+    </div>
+  );
+}
+
+export default Success;
+
--- sprint-3/client/src/components/scenes/Upload/Upload.jsx
+++ sprint-3/client/src/components/scenes/Upload/Upload.jsx
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Success from '../Success/Success';
 
 class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false,
+      errors: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   handleSubmit(e) {
     e.preventDefault();
+
+    if (!e.target.title.value || !e.target.description.value) {
+      this.setState({ errors: true });
+      return;
+    }
+
     axios.post('/videos', {
       title: e.target.title.value,
       description: e.target.description.value,
       image: "/images/upload-video-preview.jpg"
-    })
-  }
+    });
 
-  handleCancel(e) {
-    // e.preventDefault();
-    return;
+    this.setState({ submitted: true });
   }
 
   render() {
+    if (this.state.submitted) {
+      return (
+        <Success />
+      );
+    }
+
+    let errorMessage;
+    if (this.state.errors) {
+      errorMessage = <div className="upload__error-message">All fields are required</div>;
+    } else {
+      errorMessage = null;
+    }
+
     return (
       <div className="upload">
         <main className="container">
@@ -29,16 +55,27 @@ class Upload extends Component {
               <img src="/images/upload-video-preview.jpg" alt="" />
               </div>
             </div>
-            <form id="uploadForm" className="upload__form" onSubmit={this.handleSubmit}>
+            <form id="uploadForm"
+                  className="upload__form"
+                  onSubmit={this.handleSubmit}>
               <label className="upload__label" htmlFor="title">Title your video</label>
-              <input id="title" className="input upload__form-input" type="text" placeholder="Add a title to your video" />
+              <input id="title"
+                     className="input upload__form-input"
+                     type="text"
+                     placeholder="Add a title to your video" />
               <label className="upload__label" htmlFor="description">Add a video description</label>
-              <textarea id="description" className="textarea upload__form-textarea" name="description" placeholder="Add a description of your video"></textarea>
+              <textarea id="description"
+                        className="textarea upload__form-textarea"
+                        placeholder="Add a description of your video">
+              </textarea>
               <div className="divider upload__divider"></div>
               <div className="upload__form-controls">
                 <button className="cta upload__submit-button">Publish</button>
-                <div className="upload__cancel-button" onClick={this.handleCancel}>Cancel</div>
+                <div className="upload__cancel-button"
+                     onClick={() => this.props.history.push('/')}>Cancel
+                </div>
               </div>
+              <div>{errorMessage}</div>
             </form>
           </div>
         </main>
